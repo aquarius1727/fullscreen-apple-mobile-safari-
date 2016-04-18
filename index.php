b6700c34c7f33f5303ecdce788aff0a329824501
aquarius1727 <water1512@gmail.com>
2016-04-18 16:19:40 +0400

    Added files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+	
+    <meta name="apple-mobile-web-app-capable" content="yes">
+	
+	<title></title>
+  </head>
+
+  <body>
+   
+    <iframe src="file.php" style="position:fixed; width: 100%; height: 100vh; border: none; z-index: 9999;"></iframe>
+    
+  </body>
+</html>
